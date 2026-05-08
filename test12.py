9c41358e729d874d60ae42337099af585f4f0c4d
ChengYI Hung <Hcyj0302@gmail.com>
2026-05-08 14:27:25 +0800

    Test12

---
+++ test12.py
@@ -0,0 +1,13 @@
+def unrequitted_experience(lover):
+    count = len(lover)
+    
+    print(f"你總共暈過{count}個女生")
+    
+    mixed_name="+".join(lover)
+
+
+    return mixed_name
+my_belove=["Chen hai","Liu","Yan","Chen"]
+love=unrequitted_experience(my_belove)
+
+print(love)
